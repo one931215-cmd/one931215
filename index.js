66ef7fdc91be2eb726f4dc40e3040510d55af06b
one931215-cmd <one931215@gmail.com>
2026-01-03 01:25:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+function greetUser() {
+    // 1. 抓取輸入框和要顯示文字的區域
+    const inputElement = document.getElementById("userName");
+    const displayArea = document.getElementById("result");
+    
+    // 2. 取得名字
+    const name = inputElement.value;
+
+    if (name.trim() === "") {
+        alert("請先輸入名字喔！");
+    } else {
+        // 3. 直接將內容輸出到 HTML 中
+        // 這裡就是您要的：名字 + "期末一起PASS"
+        displayArea.innerHTML = `${name} 期末一起 PASS 吧！🔥`;
+        
+        // 如果想讓名字變色，可以這樣寫：
+        // displayArea.innerHTML = `<span style="color:blue;">${name}</span> 期末一起 PASS 吧！`;
+    }
+}
+
+// 您原本的其他功能
 function showMessage() {
     alert("你點擊了按鈕!");
 }
@@ -17,6 +38,3 @@ function deletebutton() {
         lastButton.remove();
     }
 }
-
-
-
